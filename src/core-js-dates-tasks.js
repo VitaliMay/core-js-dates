f0ed0c7f5ebda00e3f0cefb4f5050afc5ed6a318
VitaliMay <mvt30@mail.ru>
2024-03-12 18:18:53 +0300

    feat: task-06-getCountDaysOnPeriod

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -148,9 +148,14 @@ function getCountDaysInMonth(month, year) {
  * '2024-02-01T00:00:00.000Z', '2024-02-02T00:00:00.000Z'  => 2
  * '2024-02-01T00:00:00.000Z', '2024-02-12T00:00:00.000Z'  => 12
  */
-function getCountDaysOnPeriod(/* dateStart, dateEnd */) {
-  throw new Error('Not implemented');
+function getCountDaysOnPeriod(dateStart, dateEnd) {
+  return (
+    (Date.parse(dateEnd) - Date.parse(dateStart)) / (1000 * 60 * 60 * 24) + 1
+  );
 }
+// function getCountDaysOnPeriod(/* dateStart, dateEnd */) {
+//   throw new Error('Not implemented');
+// }
 
 /**
  * Returns true if a given date is within a specified range, including both the start and end dates.
