c8ceb098b83cb11c38e5f70c8dccaa8a4b4c861a
VitaliMay <mvt30@mail.ru>
2024-03-13 20:19:19 +0300

    feat: task-14-isLeapYear

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -409,9 +409,17 @@ function getWorkSchedule(period, countWorkDays, countOffDays) {
  * Date(2022, 2, 1) => false
  * Date(2020, 2, 1) => true
  */
-function isLeapYear(/* date */) {
-  throw new Error('Not implemented');
+function isLeapYear(date) {
+  const yearNumber = date.getFullYear();
+  return (
+    (yearNumber % 100 === 0 && yearNumber % 400 === 0) ||
+    (yearNumber % 4 === 0 && yearNumber % 100 !== 0)
+  );
 }
+
+// function isLeapYear(/* date */) {
+//   throw new Error('Not implemented');
+// }
 
 module.exports = {
   dateToTimestamp,
